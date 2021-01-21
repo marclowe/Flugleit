aa61747d192fb41fd520aadf92dd0deaee3da18d
Marc Lowe <ewolcram13@gmail.com>
2021-01-20 23:03:41 -0800

    nearly finished capstone

--- flugleit/flugleit/settings.py
+++ flugleit/flugleit/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/css/main.css/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
 
 MEDIA_URL = '/images/'
